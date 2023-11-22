e04360d439a41a01b050394545838e99ebc2de85
Jalled Omar <jalledomar2001@gmail.com>
2023-11-22 16:03:20 +0100

    deleting the inrelated files

--- src/Controller/DomaineController.php
+++ src/Controller/DomaineController.php
@@ -11,10 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/domaine')]
+/*
+ * Route('/domaine')
+ */
 class DomaineController extends AbstractController
 {
-    #[Route('/', name: 'app_domaine_index', methods: ['GET'])]
+    /*
+     * Route('/', name: 'app_domaine_index', methods: ['GET'])
+     */
     public function index(DomaineRepository $domaineRepository): Response
     {
         return $this->render('domaine/index.html.twig', [
@@ -22,7 +26,9 @@ class DomaineController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_domaine_new', methods: ['GET', 'POST'])]
+    /*
+     * Route('/new', name: 'app_domaine_new', methods: ['GET', 'POST'])
+     */
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $domaine = new Domaine();
